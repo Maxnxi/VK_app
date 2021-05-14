1492060c57c1f8a4fb46e6dc7e5ade88c626befd
Mks <nisq@ya.ru>
2021-05-14 11:57:34 +0300

    Firebase updated

---
+++ Homework_1/Login/LoginToFirebaseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginToFirebaseViewController.swift
+//  Homework_1
+//
+//  Created by Maksim on 14.05.2021.
+//
+
+import UIKit
+import Foundation
+
+class LoginToFirebaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+
+}
